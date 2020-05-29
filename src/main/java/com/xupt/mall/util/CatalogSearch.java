050c8cda73d8525297482620127899c09d5d907e
pavi-du <dmxpavi98@foxmail.com>
2020-05-29 15:54:14 +0800

    工具类

---
+++ src/main/java/com/xupt/mall/util/CatalogSearch.java
@@ -0,0 +1,15 @@
+package com.xupt.mall.util;
+
+import lombok.Data;
+
+/**
+ * TODO
+ *
+ * @author pavi
+ * @date 2020/5/28 9:02
+ */
+@Data
+public class CatalogSearch {
+
+    private String catalogName;
+}
---
+++ src/main/java/com/xupt/mall/util/SkuInfoSearch.java
@@ -0,0 +1,16 @@
+package com.xupt.mall.util;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+/**
+ * TODO
+ *
+ * @author pavi
+ * @date 2020/5/29 10:48
+ */
+@Data
+public class SkuInfoSearch {
+
+    private String skuName;
+}
