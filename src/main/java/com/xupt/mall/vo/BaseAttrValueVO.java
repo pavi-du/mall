6ddc0ec7112789f14dca6122c8f64bb43766d377
pavi-du <dmxpavi98@foxmail.com>
2020-05-29 15:54:37 +0800

    VO

---
+++ src/main/java/com/xupt/mall/vo/BaseAttrValueVO.java
@@ -0,0 +1,21 @@
+package com.xupt.mall.vo;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
+
+/**
+ * TODO
+ *
+ * @author pavi
+ * @date 2020/5/27 10:52
+ */
+@Data
+public class BaseAttrValueVO {
+
+    private Integer id;
+
+    private String baseAttrName;
+
+    private String baseAttrId;
+
+}
---
+++ src/main/java/com/xupt/mall/vo/UploadSkuInfo.java
@@ -0,0 +1,43 @@
+package com.xupt.mall.vo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.xupt.mall.entity.BaseAttr;
+import com.xupt.mall.entity.Catalog;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * TODO
+ *
+ * @author pavi
+ * @date 2020/5/28 9:58
+ */
+@Data
+public class UploadSkuInfo {
+
+    private String skuName;
+
+    private String skuPrice;
+
+    private String skuCount;
+
+    private List<String> catalogIdList = new ArrayList<>();
+
+
+    private String skuImg;
+
+    private String skuDesc;
+
+    private List<String> baseAttrIdList = new ArrayList<>();
+    private List<String> baseAttrValueIdList = new ArrayList<>();
+
+    private List<Catalog> catalogLit = new ArrayList<>();
+
+    private List<BaseAttr> baseAttrList = new ArrayList<>();
+
+
+}
